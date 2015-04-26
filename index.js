8cfb5fde6f60306f9f381724893e9e83213e2560
othree <othree@gmail.com>
2015-04-26 10:19:15 +0800

    Work code

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 'use strict';
 
 var parse = require('babel-core').transform;
+var attachComments = require('estraverse').attachComments;
 
 module.exports = function (options) {
   options = options || {};
+  options.ast = true;
+  options.code = false;
+
+  var attachComment = true;
 
   return function (files) {
     return files.map(function (file) {
       var result = parse(file.contents, options);
+      var ast = result.ast.program;
 
       return {
         type: 'File',
-        program: result.ast,
+        program: ast,
         loc: {
           source: file.path
         }
